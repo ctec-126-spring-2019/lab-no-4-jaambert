1325ab936d051368f4bab88a6974f546c1d76e98
JACOB AMBERT <jacobambert@JACOBs-MacBook-Pro.local>
2019-06-03 12:22:07 -0700

    submission finished

--- js/lab4.js
+++ js/lab4.js
@@ -2,29 +2,25 @@
 // The verse1(), verse2(), chorus1() and chorus2() are provided to you. You can change the function names and add additonal ones as needed. Be sure to follow the starter code that has been supplied.
 
 function verse1(){
-    let output = '';
-    // your code here
-
+    let output ="<p>I've paid my dues Time after time I've done my sentence But committed no crime And bad mistakes  I've made a few I've had my share of sand kicked in my face But I've come through.</p>";
+    
     return output;
 }
 
 function verse2(){
-    let output = '';
-    // your code here
-
+    let output = "<p>I've taken my bows And my curtain calls You brought me fame and fortune and everything that goes with it I thank you all But it's been no bed of roses No pleasure cruise I consider it a challenge before the whole human race and I ain't gonna lose</p>";
+    
     return output;
 }
 
 function chorus1(){
-    let output = '';
-    // your code here
-
+    let output = "<p> We are the champions, my friends And we'll keep on fighting 'til the end We are the champions We are the champions No time for losers 'Cause we are the champions of the world.</p>";
     return output;
 }
 
 function chorus2(){
-    let output = '';
-    // your code here
+    let output = "<p>We are the champions, my friends And we'll keep on fighting 'til the end We are the champions We are the champions No time for losers  'Cause we are the champions of the world.</p>";
+
 
     return output;
 }
@@ -32,12 +28,7 @@ function chorus2(){
 
 
 function main(){
-    let finalOutput = ''; // used to build lyrics string
-    // call verse and chorus functions
-    // each verse/chorus should return text including HTML paragraph tags
-
-    // do not modify the statement below
-    // it will take 
+    let finalOutput = verse1() + chorus1() + verse2() + chorus2();
     document.getElementById('lyrics').innerHTML = finalOutput;
 }
 
